22694638e112383b4f16444a68c85339958775c8
Ilya Ivanov <iliakaskad@gmail.com>
2026-01-25 20:08:27 +0300

    (feat):added sort and search layout

--- components/ui/shared/filters.tsx
+++ components/ui/shared/filters.tsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import { Title } from './title';
 import { FilterCheckbox } from './filter-checkbox';
-import { Input } from '../ui';
+import { Input, RangeSlider } from '../ui';
+import { CheckboxFiltersGroup } from './sheckbox-filters-group';
 
 interface Props {
  className?: string;
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
+ const ingredients = [
+  {
+   text: 'Сырный соус',
+   value: '1',
+  },
+  {
+   text: 'Моцарелла',
+   value: '2',
+  },
+  {
+   text: 'Чеснок',
+   value: '3',
+  },
+  {
+   text: 'Соленые огурчики',
+   value: '4',
+  },
+  {
+   text: 'Красный лук',
+   value: '5',
+  },
+  {
+   text: 'Томаты',
+   value: '6',
+  },
+  {
+   text: 'Бекон',
+   value: '7',
+  },
+  {
+   text: 'Перец',
+   value: '8',
+  },
+  {
+   text: 'Перец',
+   value: '8',
+  },
+  {
+   text: 'Перец',
+   value: '8',
+  },
+  {
+   text: 'Перец',
+   value: '8',
+  },
+ ];
+
  return (
   <div className={className}>
    <Title
@@ -15,7 +63,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
     size="sm"
     className="mb-5 font-bold"
    />
-
+   {/* Верхние чекбоксы */}
    <div className="flex flex-col gap-4">
     <FilterCheckbox
      text="Можно собирать"
@@ -26,7 +74,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
      value="2"
     />
    </div>
-
+   {/* Фильтр цен */}
    <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
     <div className="flex gap-3 mb-5">
      <Input
@@ -44,8 +92,23 @@ export const Filters: React.FC<Props> = ({ className }) => {
      />
     </div>
 
-    <RangeSlider min={0}/>
+    <RangeSlider
+     min={0}
+     max={35}
+     step={1}
+     value={[0, 35]}
+    />
    </div>
+
+   {/* Фильтр ингридиентов */}
+
+   <CheckboxFiltersGroup
+    className="mt-5"
+    title="Ингридиенты"
+    limit={6}
+    defaultItems={ingredients}
+    items={ingredients}
+   />
   </div>
  );
 };
--- components/ui/shared/index.tsx
+++ components/ui/shared/index.tsx
@@ -6,3 +6,4 @@ export { Title } from './title';
 export { Categories } from './categories';
 export { SortPopup } from './sort-popup';
 export { TopBar } from './top-bar';
+export { CheckboxFiltersGroup } from './sheckbox-filters-group';
